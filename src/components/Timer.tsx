008975144aee4ee970efb2bec21e2549d8060a9d
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-11-12 14:00:38 -0600

    Time since start logic started

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,53 @@
 import styled from "styled-components";
+import { useState } from "react";
+
+interface TimerProps {
+  time: string;
+}
+
+const Timer: React.FC<TimerProps> = ({ time }) => {
+  const [timerActive, setTimerActive] = useState(false);
+
+  const toggleTimer = () => {
+    timerActive === true
+      ? console.log("timer started!")
+      : console.log("timer stopped!");
+    setTimerActive((prev) => !prev);
+  };
+
+  const startTimer = (length: number) => {
+    const startTime = new Date();
+
+    console.log("Start Time: ", startTime);
+
+    const interval = setInterval(() => {
+      const updatedTime = new Date();
+      const timeDiff = updatedTime.getTime() - startTime.getTime();
+      const roundedTimeDiff = Math.round(timeDiff / 1000);
+
+      //Checking to see if timer has reached 25min
+      if (roundedTimeDiff === 1500) {
+        //Ending the interval timer
+        clearInterval(interval);
+      }
+
+      console.log("Seconds since start: ", roundedTimeDiff);
+    }, 1000);
+
+    return length;
+  };
+
+  startTimer(25);
+
+  return (
+    <StyledTimer onClick={() => toggleTimer()}>
+      {time}
+      <Typography>Push To Start</Typography>
+    </StyledTimer>
+  );
+};
+
+export default Timer;
 
 const StyledTimer = styled.div`
   border-radius: 50%;
@@ -13,20 +62,15 @@ const StyledTimer = styled.div`
   color: white;
   font-size: 6rem;
   font-weight: 600;
+  user-select: none;
+  box-shadow: 0 0 60px 1px #7fff;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
-interface TimerProps {
-  time: string;
-}
-
-const Timer: React.FC<TimerProps> = ({ time }) => {
-  return (
-    <StyledTimer>
-      {/* <Typography> */}
-      {time}
-      {/* </Typography> */}
-    </StyledTimer>
-  );
-};
-
-export default Timer;
+const Typography = styled.div`
+  margin-top: 1rem;
+  font-size: 1rem;
+`;
