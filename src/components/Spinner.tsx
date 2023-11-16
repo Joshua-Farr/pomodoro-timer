7c9eef0a6ad518954c735bc4f18530c44e5715c2
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-11-15 23:09:07 -0600

    Beginning dynamic timer

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,9 +2,16 @@ import styled from "styled-components";
 
 const StyledSpinner = styled.div`
   background-color: #f7706f;
+  width: 250px;
+  height: 250px;
+  border-radius: 50%;
 `;
 
-const Spinner = (percent: number) => {
+type SpinnerProps = {
+  percent: number;
+};
+
+const Spinner = (percent: SpinnerProps) => {
   return <StyledSpinner></StyledSpinner>;
 };
 
