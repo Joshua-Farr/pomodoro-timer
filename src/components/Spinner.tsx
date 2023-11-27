738bb9ec1f86414a98cdf62ed9485e7681499fec
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-11-26 18:22:07 -0600

    Working on the spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 
 const StyledSpinner = styled.div`
-  background-color: #f7706f;
+  // background: conic-gradient(#f7706f, 3.6deg, #ededed, 0deg);
   width: 250px;
   height: 250px;
-  border-radius: 50%;
+  // border: 15px solid #f7706f;
+  // border-radius: 50%;
+  fill: none;
+  stroke: #f7706f;
+  stroke-width: 20px;
 `;
 
 type SpinnerProps = {
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import Spinner from "./components/Spinner.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    {/* <Spinner percent={10} /> */}
+    {/* <App /> */}
+    <Spinner percent={10} />
   </React.StrictMode>
 );
